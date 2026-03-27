3952c7c389f6efb2bcb439b15b09ab69ebf9c6fd
Shanu Jadoun <shanujadoun604@gmail.com>
2026-03-27 20:53:24 +0530

    Todo app v1 (CRUD + localStorage)

---
+++ script.js
@@ -0,0 +1,109 @@
+let todos = [];
+
+
+// get the todos from local storage
+function getTodo() {
+    const data = localStorage.getItem("todo")
+    if (data) {
+        return JSON.parse(data)
+    } else {
+        return []
+    }
+
+
+}
+todos = getTodo()
+rendertodos()
+
+
+
+// add todo
+function addTodo() {
+    const input = document.getElementById("todo-input")
+    const text = input.value.trim()
+    if (text === "") return;
+    const newtodo = {
+        id: Date.now(),
+        text: text,
+        completed: false
+
+    }
+    todos.push(newtodo)
+    rendertodos()
+    saveTodo()
+    input.value = ""
+
+
+
+
+}
+function saveTodo() {
+    localStorage.setItem("todo", JSON.stringify(todos))
+}
+
+
+const addBTN = document.getElementById("add-btn")
+addBTN.addEventListener("click", () => {
+    addTodo()
+
+})
+
+
+
+
+
+
+
+function rendertodos() {
+    // get the todo list
+    const list = document.getElementById("todo-list")
+    // clear the list
+    list.innerHTML = ""
+    // render the todos
+    todos.forEach((todo) => {
+        //wrapper 
+        const div = document.createElement("div")
+        div.className = "flex items-center justify-between border p-2 rounded"
+        //left side
+        const left = document.createElement("div")
+        left.className = "flex items-center gap-2"
+
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.checked = todo.completed
+
+        const text = document.createElement("span")
+        text.innerText = todo.text
+
+        checkbox.addEventListener("change", () => {
+            todo.completed = checkbox.checked
+            rendertodos()
+            saveTodo()
+        })
+        if (todo.completed) {
+            text.classList.add("line-through", "text-gray-400");
+        }
+
+        //right side/delete button
+        const delBtn = document.createElement("button")
+        delBtn.className = "px-4 bg-red-500 hover:bg-red-700  text-white rounded cursor-pointer"
+        delBtn.innerText = "Delete"
+        // delBtn.className = "text-red-500"
+        delBtn.addEventListener("click", () => {
+            todos = todos.filter((t) => t.id !== todo.id)
+            rendertodos()
+            saveTodo()
+        })
+
+        //structure
+        left.appendChild(checkbox)
+        left.appendChild(text)
+
+        div.appendChild(left)
+        div.appendChild(delBtn)
+
+        list.appendChild(div)
+    })
+
+
+}
